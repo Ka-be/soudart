e04b785e0312f71fb4407f0f270b120876b24cf3
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2023-04-25 11:10:31 +0200

    add hero banner

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,14 +6,16 @@ const Hero = () => {
       <div className="hero-banner">
       <video
         className="video"
-        src="./video/welder.mpeg" // Remplacez par le chemin d'accès à votre vidéo
+        src="./video/welder.mp4" // Remplacez par le chemin d'accès à votre vidéo
         autoPlay // Lecture automatique
         loop // Boucle de lecture
         muted // Lecture sans son (facultatif)
+        playbackRate={0.5}
       >
         {/* Message de secours pour les navigateurs ne prenant pas en charge la balise vidéo */}
         Votre navigateur ne prend pas en charge la balise vidéo.
       </video>
+      <h1>Soud'Art</h1>
     </div>
     </div>
   );
